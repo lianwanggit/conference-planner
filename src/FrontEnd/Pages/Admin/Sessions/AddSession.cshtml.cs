52c6432198745bb554585257b202fae78da3fa64
Lian Wang <lian.wang@enlighten.co.nz>
2017-07-31 16:52:26 +1200

    add coustom validation on the add session page

--- src/FrontEnd/Pages/Admin/Sessions/AddSession.cshtml.cs
+++ src/FrontEnd/Pages/Admin/Sessions/AddSession.cshtml.cs
@@ -32,14 +32,35 @@ namespace FrontEnd.Pages.Admin.Sessions
 
         public async Task OnGetAsync()
         {
-            var conferences = await _apiClient.GetConferencesAsync();
-            Conferences = conferences.Select(c => 
-                new SelectListItem { Value = c.ID.ToString(), Text = c.Name })
-                .ToList();
+            await GetConferences();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            var conference = await _apiClient.GetConferenceAsync(Session.ConferenceID);
+            if (conference == null)
+            {
+                await DisplayValidationError("Session.ConferenceID", "The selected conference is not available");
+            }
+            if (Session.StartTime.HasValue 
+                && conference.StartDate.HasValue
+                && Session.StartTime < conference.StartDate)
+            {
+                await DisplayValidationError("Session.StartTime", "The Session StartTime must be later than Conference StartDate");
+            }
+            if (conference.EndDate.HasValue
+                && Session.EndTime.HasValue
+                && Session.EndTime > conference.EndDate)
+            {
+                await DisplayValidationError("Session.EndTime", "The Session EndTime must be earlier than Conference EndDate");
+            }
+            if (Session.StartTime.HasValue
+                && Session.EndTime.HasValue
+                && Session.StartTime >= Session.EndTime)
+            {
+                await DisplayValidationError("Session.EndTime", "The Session EndTime must be later than StartTime");
+            }
+
             if (!ModelState.IsValid)
             {
                 return Page();
@@ -50,6 +71,20 @@ namespace FrontEnd.Pages.Admin.Sessions
             await _apiClient.AddSessionAsync(Session);
 
             return RedirectToPage();
+        }
+
+        private async Task GetConferences()
+        {
+            var conferences = await _apiClient.GetConferencesAsync();
+            Conferences = conferences.Select(c =>
+                new SelectListItem { Value = c.ID.ToString(), Text = c.Name })
+                .ToList();
+        }
+
+        private async Task DisplayValidationError(string key, string message)
+        {
+            await GetConferences();
+            ModelState.AddModelError(key, message);
         }
     }
 }
--- src/FrontEnd/Pages/Admin/Sessions/EditSession.cshtml.cs
+++ src/FrontEnd/Pages/Admin/Sessions/EditSession.cshtml.cs
@@ -71,5 +71,11 @@ namespace FrontEnd.Pages.Admin
 
             return RedirectToPage("/Index");
         }
+
+        private async Task DisplayValidationError(string key, string message)
+        {
+            //await GetConferences();
+            ModelState.AddModelError(key, message);
+        }
     }
 }
